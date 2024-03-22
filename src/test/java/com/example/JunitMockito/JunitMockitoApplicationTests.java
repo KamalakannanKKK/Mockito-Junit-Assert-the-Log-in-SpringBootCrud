7f7dd147a18560ea3c65e20de24e44a6ba9296b7
KamalakannanKKK <109393676+KamalakannanKKK@users.noreply.github.com>
2024-03-22 11:46:38 +0530

    Update JunitMockitoApplicationTests.java

--- src/test/java/com/example/JunitMockito/JunitMockitoApplicationTests.java
+++ src/test/java/com/example/JunitMockito/JunitMockitoApplicationTests.java
@@ -29,37 +29,5 @@ import com.example.JunitMockito.Services.vendorService;
 @SpringBootTest
 class JunitMockitoApplicationTests {
 
-	private MockMvc mockmvc;
-	
-	@Mock
-	vendorService vendorService;
-	
-	@Mock
-	VendorRepo repo;
-
-	
-	Controller controller;
-	
-	Vendor vendor1=new Vendor(1,"Amazon","USA",987654433);
-	Vendor vendor2=new Vendor(2,"Flipkart","UK",98766478);
-	Vendor vendor3=new Vendor(3,"EBay","km",987656788);
-	
-	@BeforeEach
-	public void setUp() {
-		MockitoAnnotations.openMocks(this);
-		this.mockmvc = MockMvcBuilders.standaloneSetup(controller).build();
-	}
-	
-	@Test
-	void getAllVendor() throws Exception {
-
-		List<Vendor> vendor = new ArrayList<Vendor>(Arrays.asList(vendor1, vendor2, vendor3));
-
-		when(repo.findAll()).thenReturn(vendor);
-
-		mockmvc.perform(MockMvcRequestBuilders.get("/getAllVendor").contentType(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk()).andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(3)));
-
-	}
 
 }
